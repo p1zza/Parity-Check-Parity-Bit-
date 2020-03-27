42ba353d51de52be7faf68500bd275b45dc05a70
Vova Ragoulin <vovaragulin@gmail.com>
2020-03-27 11:05:23 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Krypt_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Result = new System.Windows.Forms.Label();
+            this.GetResult = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(55, 65);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(100, 20);
+            this.NameBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите Имя";
+            // 
+            // Result
+            // 
+            this.Result.AutoSize = true;
+            this.Result.Location = new System.Drawing.Point(70, 150);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(0, 13);
+            this.Result.TabIndex = 2;
+            // 
+            // GetResult
+            // 
+            this.GetResult.Location = new System.Drawing.Point(73, 101);
+            this.GetResult.Name = "GetResult";
+            this.GetResult.Size = new System.Drawing.Size(75, 23);
+            this.GetResult.TabIndex = 3;
+            this.GetResult.Text = "button1";
+            this.GetResult.UseVisualStyleBackColor = true;
+            this.GetResult.Click += new System.EventHandler(this.GetResult_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 315);
+            this.Controls.Add(this.GetResult);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NameBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Result;
+        private System.Windows.Forms.Button GetResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Krypt_8
+{
+    public partial class Form1 : Form
+    {
+        public List<int> even = new List<int>();
+        public List<int> odd = new List<int>();
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GetResult_Click(object sender, EventArgs e)
+        {
+            string name = NameBox.Text;
+            var s = StrToAsciiArray(name);
+            List<string> vs = new List<string>();
+            var d = ToBinary(vs, s);
+
+            d = GetParitetBytes(d);
+
+            Result.Text = name + even.ToString() + "\n"+ odd.ToString();
+
+
+            var mas_even = even.ToArray();
+            var mas_odd = odd.ToArray();
+            var mas_d = d.ToArray();
+
+            string res = "";
+            res += "| " + "Входная строка"+ " | " + "even" + " | " + "odd" + " |\n";
+
+            for (int i = 0; i < name.Length;i++)
+            {
+                res += "|      " + mas_d[i] + "       |    " +mas_even[i]+ "    |   " + mas_odd[i] + "   |\n";
+            }
+
+            Result.Text = res;
+        }
+
+        
+        
+
+        private List<string> GetParitetBytes(List<string> input)
+        {
+            int sum = 0;
+            foreach(var s in input)
+            {
+                s.ToCharArray();
+                for(int i=0;i<s.Length;i++)
+                {
+                    sum += Convert.ToInt32(s[i].ToString());
+                }
+                even.Add(sum % 2);
+                if (sum%2 == 1)
+                {
+                    odd.Add(0);
+                }
+                else
+                {
+                    odd.Add(1);
+                }
+            }
+
+            return input;
+        }
+
+        private byte[] StrToAsciiArray(string s)
+        {
+            return Encoding.UTF8.GetBytes(s.ToCharArray());
+        }
+
+        private List<string> ToBinary(List<string> input, byte[] bin_input)
+        {
+            foreach (var s in bin_input)
+            {
+                input.Add((Convert.ToString(s, 2).PadLeft(8, '0')));
+            }
+            return input;
+        }
+    }
+
+
+}
